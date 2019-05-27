c3fb81af567936b3e074af12c43f6b00590e46cc
bindu8829 <51020527+bindu8829@users.noreply.github.com>
2019-05-27 12:02:07 +0530

    Add files via upload

---
+++ Arithmetic_operators.js
@@ -0,0 +1,21 @@
+function Multiply(a,b)
+{
+	return a*b;
+
+function Sub(a,b)
+{
+	return a-b;
+}
+function Div(a,b)
+{
+	return a/b;
+}
+function Add(x,y)
+{
+	return x+y;
+}
+exports.Multiply = Multiply;
+exports.Sub = Sub;
+exports.Div = Div;
+exports.Add = Add;
+
---
+++ README.md
@@ -0,0 +1,5 @@
+Arithmetic module consists of arithmetic functionalites such as
+1. Addition of 2 numbers ---- Add(a,b)
+2. Substraction of 2 numbers ---- Sub(a,b)
+3. Division of 2 numbers ---- Div(a,b)
+4. Multiplication of 2 numbers ---- Multiply(a,b)
